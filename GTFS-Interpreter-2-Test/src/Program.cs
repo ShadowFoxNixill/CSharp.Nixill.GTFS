e8e0e1e4fee14e8e04bd4e448d24e1ac559cd425
ShadowFoxNixill <github@nixill.net>
2021-05-11 00:27:44 -0400

    Cool, custom tables work :D

--- GTFS-Interpreter-2-Test/src/Program.cs
+++ GTFS-Interpreter-2-Test/src/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using Nixill.GTFS.Parsing;
 using System.Collections.Generic;
 using Nixill.GTFS.Collections;
+using NodaTime;
 
 namespace Nixill.Testing
 {
@@ -13,7 +14,7 @@ namespace Nixill.Testing
   {
     static void Main(string[] args)
     {
-      var source = new ZipGTFSDataSource("smart_gtfs.zip");
+      var source = new ZipGTFSDataSource("gtfs/ddot_gtfs.zip");
       StopTimeTest(source);
     }
 
@@ -22,7 +23,32 @@ namespace Nixill.Testing
       GTFSFeed feed = new GTFSFeed(source);
       var stopTimes = new TwoKeyEntityCollection<string, int, StopTime>(source, "stop_times", StopTime.Factory);
 
-      
+      var tripsByRouteService = feed.Trips.GroupBy(tp => (tp.RouteId, tp.ServiceId));
+      var timesByTrip = stopTimes.GroupBy(stm => stm.TripID).ToDictionary(stm => stm.Key, stm => new { start = stm.Min(x => x.DepartureTime), end = stm.Max(x => x.ArrivalTime) });
+
+      DurationPattern ptn = DurationPattern.CreateWithInvariantCulture("H:mm:ss");
+
+      foreach (var group in tripsByRouteService)
+      {
+        var rt = feed.Routes[group.Key.RouteId];
+        var cal = feed.Calendars[group.Key.ServiceId].Item1;
+
+        string header = $"{rt.Type} {rt.ShortName} {rt.LongName} - {DayMasks.Get(cal.Mask)}";
+
+        Duration min = Duration.MaxValue;
+        Duration max = Duration.MinValue;
+
+        foreach (var trip in group)
+        {
+          var times = timesByTrip[trip.ID];
+          min = Duration.Min(times.start.Value, min);
+          max = Duration.Max(times.end.Value, max);
+        }
+
+        string timeStr = $"{ptn.Format(min)} - {ptn.Format(max)}";
+
+        Console.WriteLine($"{header}: {timeStr}");
+      }
     }
 
     private static void StopTest(GTFSFeed feed)
