b83c13ca206fd890513bcef45f30b9acf8e9a2e0
Nixill <github@nixill.net>
2021-06-03 14:40:20 -0400

    Some work on GTFSOrderedEntityCollection

--- GTFS-Interpreter-2-Proj/src/GTFS/Collections/GTFSOrderedEntityCollection.cs
+++ GTFS-Interpreter-2-Proj/src/GTFS/Collections/GTFSOrderedEntityCollection.cs
@@ -1,43 +1,136 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using Nixill.Collections;
 using Nixill.GTFS.Entities;
+using Nixill.GTFS.Parsing;
 
-namespace Nixill.GTFS
+namespace Nixill.GTFS.Collections
 {
-  public class GTFSOrderedEntityCollection<T> : IReadOnlyDictionary<(string, int), T> where T : GTFSOrderedEntity
+  public class GTFSOrderedEntityCollection<T> : IReadOnlyCollection<T> where T : GTFSOrderedEntity
   {
     private Dictionary<string, AVLTreeDictionary<int, T>> Backing;
+    private List<GTFSUnparsedEntity> Unparsed;
 
-    public
+    /// <summary>
+    ///   The number of entities within this collection.
+    /// </summary>
+    public int Count { get; }
 
-    public T this[(string, int) key] => throw new System.NotImplementedException();
-
-    public IEnumerable<(string, int)> Keys => throw new System.NotImplementedException();
-
-    public IEnumerable<T> Values => throw new System.NotImplementedException();
-
-    public int Count => throw new System.NotImplementedException();
-
-    public bool ContainsKey((string, int) key)
+    /// <summary>
+    ///   Creates a new GTFSOrderedEntityCollection from the given table in the
+    ///   given feed.
+    /// </summary>
+    public GTFSOrderedEntityCollection(IGTFSDataSource source, string tableName, GTFSEntityFactory<T> factory)
     {
-      throw new System.NotImplementedException();
+      Backing = new Dictionary<string, AVLTreeDictionary<int, T>>();
+      var backingGen = new DictionaryGenerator<string, AVLTreeDictionary<int, T>>(Backing,
+        new FuncGenerator<string, AVLTreeDictionary<int, T>>(x => new AVLTreeDictionary<int, T>()));
+      Unparsed = new List<GTFSUnparsedEntity>();
+
+      foreach (T item in source.GetObjects(tableName, factory, Unparsed))
+      {
+        backingGen[item.ID].Add(item.Index, item);
+      }
     }
 
-    public IEnumerator<KeyValuePair<(string, int), T>> GetEnumerator()
+    /// <summary>
+    ///   Creates a new IDEntityCollection from the given collection of
+    ///   existing objects.
+    /// </summary>
+    public GTFSOrderedEntityCollection(ICollection<T> objects)
     {
-      throw new System.NotImplementedException();
+      Backing = new Dictionary<string, AVLTreeDictionary<int, T>>();
+      var backingGen = new DictionaryGenerator<string, AVLTreeDictionary<int, T>>(Backing,
+        new FuncGenerator<string, AVLTreeDictionary<int, T>>(x => new AVLTreeDictionary<int, T>()));
+      Unparsed = new List<GTFSUnparsedEntity>();
+
+      foreach (T item in objects)
+      {
+        backingGen[item.ID].Add(item.Index, item);
+      }
     }
 
-    public bool TryGetValue((string, int) key, [MaybeNullWhen(false)] out T value)
+    /// <summary>
+    ///   Returns <c>true</c> iff the collection contains the given entity.
+    /// </summary>
+    /// <remarks>
+    ///   More specifically, this is <c>true</c> iff the collection has a
+    ///   key that matches the given entity's ID and index.
+    /// </remarks>
+    public bool Contains(T item) => Backing.TryGetValue(item.ID, out var middle) && middle.ContainsKey(item.Index);
+
+    /// <summary>
+    ///   Returns <c>true</c> iff the collection contains a given key.
+    /// </summary>
+    public bool Contains((string ID, int Index) key) =>
+      Backing.TryGetValue(key.ID, out var middle) && middle.ContainsKey(key.Index);
+
+    /// <summary>
+    ///   Returns <c>true</c> iff the collection contains a given collection.
+    /// </summary>
+    /// <remarks>
+    ///   More specifically, returns <c>true</c> iff the collection
+    ///   contains any entity with the given ID.
+    /// </remarks>
+    public bool Contains(string key) => Backing.ContainsKey(key);
+
+    /// <summary>
+    ///   Returns <c>true</c> iff the collection contains a key lower than
+    ///   a given key.
+    /// </summary>
+    /// <remarks>
+    ///   More specifically, this is <c>true</c> iff the collection has a
+    ///   key that matches the given ID and has a lower Index.
+    /// </remarks>
+    public bool ContainsLower((string ID, int Index) key) =>
+      Backing.TryGetValue(key.ID, out var middle) && middle.ContainsLower(key.Index);
+
+    /// <summary>
+    ///   Returns <c>true</c> iff the collection contains a key lower than
+    ///   or equal to a given key.
+    /// </summary>
+    /// <remarks>
+    ///   More specifically, this is <c>true</c> iff the collection has a
+    ///   key that matches the given ID and has an equal or lower Index.
+    /// </remarks>
+    public bool ContainsFloor((string ID, int Index) key) =>
+      Backing.TryGetValue(key.ID, out var middle) && middle.ContainsFloor(key.Index);
+
+    /// <summary>
+    ///   Returns <c>true</c> iff the collection contains a key higher
+    ///   than or equal to a given key.
+    /// </summary>
+    /// <remarks>
+    ///   More specifically, this is <c>true</c> iff the collection has a
+    ///   key that matches the given ID and has an equal or higher Index.
+    /// </remarks>
+    public bool ContainsCeiling((string ID, int Index) key) =>
+      Backing.TryGetValue(key.ID, out var middle) && middle.ContainsCeiling(key.Index);
+
+    /// <summary>
+    ///   Returns <c>true</c> iff the collection contains a key higher
+    ///   than a given key.
+    /// </summary>
+    /// <remarks>
+    ///   More specifically, this is <c>true</c> iff the collection has a
+    ///   key that matches the given ID and has a higher Index.
+    /// </remarks>
+    public bool ContainsHigher((string ID, int Index) key) =>
+      Backing.TryGetValue(key.ID, out var middle) && middle.ContainsHigher(key.Index);
+
+    private IEnumerable<T> Enumerable()
     {
-      throw new System.NotImplementedException();
+      foreach (AVLTreeDictionary<int, T> dict in Backing.Values)
+      {
+        foreach (T val in dict.Values)
+        {
+          yield return val;
+        }
+      }
     }
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-      throw new System.NotImplementedException();
-    }
+    public IEnumerator<T> GetEnumerator() => Enumerable().GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => Enumerable().GetEnumerator();
   }
 }
